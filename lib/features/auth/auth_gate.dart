6d3dec2776c3ebc7c11de3bee25f3c208252b048
Klaudia Bąk <aoimesan@gmail.com>
2023-04-04 18:25:47 +0200

    Install Auth Gate

---
+++ lib/features/auth/auth_gate.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutterfire_ui/auth.dart';
+
+import '../../home/home_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      initialData: FirebaseAuth.instance.currentUser,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration(),
+            ],
+          );
+        }
+
+        return const HomePage(
+          title: '',
+          // currentUser: snapshot.data!,
+        );
+      },
+    );
+  }
+}
